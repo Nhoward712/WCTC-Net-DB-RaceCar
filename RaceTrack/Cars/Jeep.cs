c9267b0f298cf3d5be8d08aacf722c6fba6276a0
Neal Howard <nhoward72@yahoo.com>
2020-03-09 21:04:17 -0500

    Test submit

---
+++ RaceTrack/Cars/Jeep.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceTrack.RaceTrack.Cars
+{
+    class Jeep : RaceCar
+    {
+        public Jeep()
+        {
+            Name = "Jeep Wrangler Rubicon";
+            TopSpeed = 80;
+        }
+
+        public override void Brake()
+        {
+            Console.WriteLine($"The {Name} drives over all of the other cars at the finish line!");
+            base.Brake();
+        }
+        public override void StartEngine()
+        {
+            Console.WriteLine($"The {Name} puts it in 4 wheel drive!");
+        }
+    }
+}
---
+++ RaceTrack/Drivers/Mario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RaceTrack.RaceTrack.Cars;
+
+namespace RaceTrack.RaceTrack.Drivers
+{
+    public class Mario : Driver
+    {
+        public Mario(RaceCar car) : base(car)
+        {
+            Name = "Mario";
+            SkillLevel = 11;
+        }
+
+        public override void Drive()
+        {
+            Car.Accelerate(SkillLevel);
+        }
+
+    }
+}
